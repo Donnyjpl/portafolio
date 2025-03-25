3351e464d834ab783895bea79f8ad69bdddd22a0
donnyjpl <donnyjpl@gmail.com>
2025-03-25 20:29:43 -0300

    false

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')  # Cargar desde el archivo .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['donniplaza.com', 'www.donniplaza.com', '127.0.0.1','localhost','cv852lcb4alc8u6tur1g.147.93.67.92']
 
